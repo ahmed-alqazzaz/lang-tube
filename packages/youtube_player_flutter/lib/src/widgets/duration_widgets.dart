a6e5e23ecdc76cdcde3e8c822f6551b518d10f21
stevehighly0 <stevehighly0>
2023-07-25 03:04:51 +0300

    updated duration widgets

--- packages/youtube_player_flutter/lib/src/widgets/duration_widgets.dart
+++ packages/youtube_player_flutter/lib/src/widgets/duration_widgets.dart
@@ -3,9 +3,7 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
-
-import '../utils/duration_formatter.dart';
-import '../utils/youtube_player_controller.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 /// A widget which displays the current position of the video.
 class CurrentPosition extends StatefulWidget {
@@ -13,10 +11,10 @@ class CurrentPosition extends StatefulWidget {
   final YoutubePlayerController? controller;
 
   /// Creates [CurrentPosition] widget.
-  CurrentPosition({this.controller});
+  const CurrentPosition({super.key, this.controller});
 
   @override
-  _CurrentPositionState createState() => _CurrentPositionState();
+  State<CurrentPosition> createState() => _CurrentPositionState();
 }
 
 class _CurrentPositionState extends State<CurrentPosition> {
@@ -52,10 +50,14 @@ class _CurrentPositionState extends State<CurrentPosition> {
 
   @override
   Widget build(BuildContext context) {
+    final currentDuration = durationFormatter(
+      _controller.value.position.inMilliseconds,
+    );
+    final videoDuration = durationFormatter(
+      _controller.value.metaData.duration.inMilliseconds,
+    );
     return Text(
-      durationFormatter(
-        _controller.value.position.inMilliseconds,
-      ),
+      '$currentDuration / $videoDuration',
       style: const TextStyle(
         color: Colors.white,
         fontSize: 12.0,
@@ -64,60 +66,29 @@ class _CurrentPositionState extends State<CurrentPosition> {
   }
 }
 
-/// A widget which displays the remaining duration of the video.
-class RemainingDuration extends StatefulWidget {
-  /// Overrides the default [YoutubePlayerController].
-  final YoutubePlayerController? controller;
-
-  /// Creates [RemainingDuration] widget.
-  RemainingDuration({this.controller});
-
-  @override
-  _RemainingDurationState createState() => _RemainingDurationState();
-}
-
-class _RemainingDurationState extends State<RemainingDuration> {
-  late YoutubePlayerController _controller;
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    final controller = YoutubePlayerController.of(context);
-    if (controller == null) {
-      assert(
-        widget.controller != null,
-        '\n\nNo controller could be found in the provided context.\n\n'
-        'Try passing the controller explicitly.',
-      );
-      _controller = widget.controller!;
-    } else {
-      _controller = controller;
-    }
-    _controller.removeListener(listener);
-    _controller.addListener(listener);
-  }
-
-  @override
-  void dispose() {
-    _controller.removeListener(listener);
-    super.dispose();
-  }
-
-  void listener() {
-    if (mounted) setState(() {});
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      "- ${durationFormatter(
-        (_controller.metadata.duration.inMilliseconds) -
-            (_controller.value.position.inMilliseconds),
-      )}",
-      style: const TextStyle(
-        color: Colors.white,
-        fontSize: 12.0,
-      ),
-    );
-  }
+/// Formats duration in milliseconds to xx:xx:xx format.
+String durationFormatter(int milliSeconds) {
+  var seconds = milliSeconds ~/ 1000;
+  final hours = seconds ~/ 3600;
+  seconds = seconds % 3600;
+  var minutes = seconds ~/ 60;
+  seconds = seconds % 60;
+  final hoursString = hours >= 10
+      ? '$hours'
+      : hours == 0
+          ? '00'
+          : '0$hours';
+  final minutesString = minutes >= 10
+      ? '$minutes'
+      : minutes == 0
+          ? '00'
+          : '0$minutes';
+  final secondsString = seconds >= 10
+      ? '$seconds'
+      : seconds == 0
+          ? '00'
+          : '0$seconds';
+  final formattedTime =
+      '${hoursString == '00' ? '' : '$hoursString:'}$minutesString:$secondsString';
+  return formattedTime;
 }
