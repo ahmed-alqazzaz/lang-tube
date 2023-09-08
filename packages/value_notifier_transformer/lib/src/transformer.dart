3d7376a170a4444740683b25684fc44753e743d3
stevehighly0 <stevehighly0>
2023-09-08 13:12:08 +0200

    updated value notifier transformer

--- packages/value_notifier_transformer/lib/src/transformer.dart
+++ packages/value_notifier_transformer/lib/src/transformer.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 import 'package:flutter/foundation.dart';
 
 extension ValueNotifierTransformer<T> on ValueNotifier<T> {
@@ -9,6 +8,7 @@ extension ValueNotifierTransformer<T> on ValueNotifier<T> {
   /// This acts like [map], except that [convert] may return a [Future],
   /// and in that case, this method waits for that future to complete before
   /// updating the result ValueNotifier.
+
   Future<ValueNotifier<R>> asyncMap<R>(
       FutureOr<R> Function(T event) mapper) async {
     final resultNotifier = ValueNotifier<R>(await mapper(value));
@@ -47,6 +47,68 @@ extension ValueNotifierTransformer<T> on ValueNotifier<T> {
 
     void updateResult(T value) {
       if (predicate(value)) {
+        resultNotifier.value = value;
+      }
+    }
+
+    // Initial update
+    updateResult(value);
+
+    // Listen to changes in the source ValueNotifier
+    addListener(() => updateResult(value));
+    return resultNotifier;
+  }
+
+  ValueNotifier<T> get unique {
+    final resultNotifier = ValueNotifier<T>(value);
+    T previousValue = value;
+
+    void updateResult(T value) {
+      if (value != previousValue) {
+        resultNotifier.value = value;
+        previousValue = value;
+      }
+    }
+
+    // Initial update
+    updateResult(value);
+
+    // Listen to changes in the source ValueNotifier
+    addListener(() => updateResult(value));
+    return resultNotifier;
+  }
+
+  ValueNotifier<T> delayWhen(
+      Duration delayDuration, bool Function(T event) shouldDelay) {
+    final resultNotifier = ValueNotifier<T>(value);
+
+    void updateResult(T value) async {
+      if (shouldDelay(value)) {
+        await Future.delayed(delayDuration);
+      }
+      resultNotifier.value = value;
+    }
+
+    // Initial update
+    updateResult(value);
+
+    // Listen to changes in the source ValueNotifier
+    addListener(() => updateResult(value));
+    return resultNotifier;
+  }
+
+  ValueNotifier<T> delayFirst(Duration duration) {
+    assert(duration >= Duration.zero, 'Duration must be non-negative.');
+
+    final resultNotifier = ValueNotifier<T>(value);
+    final timer = Stopwatch()..start();
+
+    void updateResult(T value) {
+      if (timer.elapsed < duration) {
+        Timer(duration - timer.elapsed, () {
+          resultNotifier.value = value;
+        });
+      } else {
         resultNotifier.value = value;
       }
     }
