2c0f0dd0d41f8cd06ed23e354cfdc90c4050fb73
ahmed.alqazaz <ahmed.alqazaz@snono-systems.com>
2023-10-28 23:29:48 +0300

    Updated Package: Languages converted factory constructors to getters + added additional methods

--- packages/languages/lib/src/languages.dart
+++ packages/languages/lib/src/languages.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:languages/src/language_flags.dart';
 
@@ -19,31 +18,72 @@ class Language {
   final String name;
   final Image flag;
 
-  factory Language.english() => Language._(
+  static Language get english => Language._(
         code: LanguageCodes.english,
         name: 'english',
         pattern: LanguagePatterns.english,
         flag: LangaugeFlags.english,
       );
 
-  factory Language.german() => Language._(
+  static Language get german => Language._(
         code: LanguageCodes.german,
         name: 'german',
         pattern: LanguagePatterns.german,
         flag: LangaugeFlags.german,
       );
 
-  factory Language.french() => Language._(
+  static Language get french => Language._(
         code: LanguageCodes.french,
         name: 'french',
         pattern: LanguagePatterns.french,
         flag: LangaugeFlags.french,
       );
 
-  factory Language.arabic() => Language._(
+  static Language get arabic => Language._(
         code: LanguageCodes.arabic,
         name: 'arabic',
         pattern: LanguagePatterns.arabic,
         flag: LangaugeFlags.arabic,
       );
+
+  static List<Language> get values => [
+        Language.arabic,
+        Language.english,
+        Language.french,
+        Language.german,
+      ];
+
+  @override
+  String toString() {
+    return 'Language(code: $code, pattern: $pattern, name: $name, flag: $flag)';
+  }
+
+  @override
+  bool operator ==(covariant Language other) {
+    if (identical(this, other)) return true;
+
+    return other.code == code &&
+        other.pattern == pattern &&
+        other.name == name &&
+        other.flag == flag;
+  }
+
+  @override
+  int get hashCode {
+    return code.hashCode ^ pattern.hashCode ^ name.hashCode ^ flag.hashCode;
+  }
+
+  Language copyWith({
+    String? code,
+    String? pattern,
+    String? name,
+    Image? flag,
+  }) {
+    return Language._(
+      code: code ?? this.code,
+      pattern: pattern ?? this.pattern,
+      name: name ?? this.name,
+      flag: flag ?? this.flag,
+    );
+  }
 }
