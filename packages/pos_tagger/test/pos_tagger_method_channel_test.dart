3135ff34a878277362155edfc62f421ff95c34e8
ahmed-alqazzaz <ahmedmohammed9198727821@gmail.com>
2024-01-23 15:16:53 +0300

    added pos tagger

---
+++ packages/pos_tagger/test/pos_tagger_method_channel_test.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:pos_tagger/pos_tagger_method_channel.dart';
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  MethodChannelPosTagger platform = MethodChannelPosTagger();
+  const MethodChannel channel = MethodChannel('pos_tagger');
+
+  setUp(() {
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(
+      channel,
+      (MethodCall methodCall) async {
+        return '42';
+      },
+    );
+  });
+
+  tearDown(() {
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(channel, null);
+  });
+
+  test('getPlatformVersion', () async {
+    expect(await platform.getPlatformVersion(), '42');
+  });
+}
